309b8a95641ab402fb084f1ffd800fc9d65587cf
polinanedilenko1 <52549518+polinanedilenko1@users.noreply.github.com>
2019-07-05 14:13:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = srt(name) + "is your best friend"
-	return render_template('index.html', name=new_name)
+	return render_template('index.html', name=name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
